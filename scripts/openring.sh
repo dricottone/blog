61beddd07fabc8ae0ee8a011d1971fa797f143b1
Dominic Ricottone <me@dominic-ricottone.com>
2023-11-11 19:19:45 +0000

    Add Ludicity

--- scripts/openring.sh
+++ scripts/openring.sh
@@ -11,5 +11,6 @@ openring \
   -s https://andrewkelley.me/rss.xml \
   -s https://christianbrickhouse.com/feed.xml \
   -s https://andreabergia.com/post/index.xml \
+  -s https://ludic.mataroa.blog/rss/ \
   < scripts/openring.html
 
